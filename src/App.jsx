e2b075440c4bed9e3814514fd4ffc7e041ea379b
tavi999 <taviposta@gmail.com>
2025-11-11 23:40:24 +0200

    Initial commit, startup project structure, a simple Board component of 3x3 squares

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+export default function Board() {
+    return (
+        <>
+            <div className="board-row">
+                <button className="square"></button>
+                <button className="square"></button>
+                <button className="square"></button>
+            </div>
+            <div className="board-row">
+                <button className="square"></button>
+                <button className="square"></button>
+                <button className="square"></button>
+            </div>
+            <div className="board-row">
+                <button className="square"></button>
+                <button className="square"></button>
+                <button className="square"></button>
+            </div>
+        </>
+    )
+}
+
+

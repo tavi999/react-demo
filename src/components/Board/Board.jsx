c2fd0f2e81583dfa72bce8700b6353ef75ace664
tavi999 <taviposta@gmail.com>
2025-11-12 09:05:12 +0200

    Updated Board.jsx component, added a calculateWinner function a condition inside handleClick to return if a winner has been decided or the board has been filled, added an xIsNext bool constant that alternates on every click and a condition so that depending on it's state to set the square to X or 0, added a winner constant and a let status with the winner string and returned it as div status

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -3,16 +3,35 @@ import {Square} from "../Square/Square.jsx";
 import {useState} from "react";
 
 export function Board() {
+    const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
 
     function handleClick(i) {
         const nextSquares = squares.slice();
-        nextSquares[i] = "X";
+
+        if(calculateWinner(squares) || squares[i]){
+            return;
+        }
+        if(xIsNext) {
+            nextSquares[i] = "X";
+        }else{
+            nextSquares[i] = "O";
+        }
         setSquares(nextSquares);
+        setXIsNext(!xIsNext);
+    }
+
+    const winner = calculateWinner(squares);
+    let status;
+    if(winner){
+        status = "Winner: " + winner;
+    }else{
+        status = "Next player: " + (xIsNext ? "X" : "O");
     }
 
     return (
         <>
+            <div className="status">{status}</div>
             <div className="board-row">
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -30,4 +49,25 @@ export function Board() {
             </div>
         </>
     )
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+
+    for(let i = 0; i < lines.length; i++){
+        const [a,b,c] = lines[i];
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+            return squares[a];
+        }
+    }
+    return null;
 }
